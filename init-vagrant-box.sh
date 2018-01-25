98edec390092a009a591fada0028dc0554d679c9
Swapnil Kulkarni <me@coolsvap.net>
2018-01-25 14:36:40 +0530

    Initial K8s commit

--- init-vagrant-box.sh
+++ init-vagrant-box.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
+
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
 yum -y install epel-release
 yum -y update
 yum -y install git net-tools wget
+
+setenforce 0
+sed -i 's/SELINUX=enforcing/SELINUX=enforcing/g' /etc/selinux/config
+swapoff -a
+sed -i '/swap/s/^/#/g' /etc/fstab
+echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
+
 rm /home/vagrant/.ssh/authorized_keys
 wget https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub -O /home/vagrant/.ssh/authorized_keys
 chown -Rf vagrant:vagrant /home/vagrant/.ssh
